5ce28c199897fc1d174bc5d67df5427db0046931
obzva <dngyng1000@gmail.com>
2024-02-20 17:17:12 +0900

    Ep 02 - Part 1 - (1 of 7) Intro, Basic Setup, Trying API

---
+++ ep02/src/main.js
@@ -0,0 +1,11 @@
+async function main() {
+  const response = await fetch(
+    "https://learnwitheunjae.dev/api/sinabro-js/ecommerce"
+  );
+
+  const json = await response.json();
+
+  console.log(json);
+}
+
+main();
