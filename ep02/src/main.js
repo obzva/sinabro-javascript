bdbfd4c1ee3ff04fa8850031a0319a74a721aee1
obzva <dngyng1000@gmail.com>
2024-02-26 11:44:01 +0900

    Ep 02 - Part 1 - (3 of 7) Advanced event handling

--- ep02/src/main.js
+++ ep02/src/main.js
@@ -5,22 +5,36 @@ async function main() {
 
   document.querySelector("#products").innerHTML = products
     .map(
-      (product) => `
-    <div class="product">
+      (product, index) => `
+    <div class="product" data-product-id="${product.id}" data-product-index="${index}">
       <img src="${product.images[0]}" alt="Image-${product.name}">
       <p>${product.name}</p>
       <div class="flex items-center justify-between">
         <span>Price: ${product.regularPrice}</span>
         <div>
-          <button type="button" class="bg-green-200 text-green-800 hover:bg-green-300 px-3 py-1 rounded-full">-<button>
+          <button type="button" class="btn-decrease bg-green-200 text-green-800 hover:bg-green-300 px-3 py-1 rounded-full">-<button>
           <span class="text-green-800 hidden">3</span>
-          <button type="button" class="bg-green-200 text-green-800 hover:bg-green-300 px-3 py-1 rounded-full">+<button>
+          <button type="button" class="btn-increase bg-green-200 text-green-800 hover:bg-green-300 px-3 py-1 rounded-full">+<button>
         </div>
       </div>
     </div>
   `
     )
     .join("");
+
+  document.querySelector("#products").addEventListener("click", (e) => {
+    const targetElement = e.target;
+    const productElement = findElement(targetElement, ".product");
+    // const productId = productElement.getAttribute("data-product-id");
+    const productIndex = productElement.getAttribute("data-product-index");
+    const product = products[productIndex];
+
+    if (targetElement.matches(".btn-decrease")) {
+      console.log("🍎 decrease!");
+    } else if (targetElement.matches(".btn-increase")) {
+      console.log("🍏 increase!");
+    }
+  });
 }
 
 async function getProducts() {
@@ -31,6 +45,17 @@ async function getProducts() {
     "https://learnwitheunjae.dev/api/sinabro-js/ecommerce"
   );
   return await response.json();
+}
+
+function findElement(startingElement, selector) {
+  let currentElement = startingElement;
+
+  while (currentElement !== null) {
+    if (currentElement.matches(selector)) return currentElement;
+    currentElement = currentElement.parentElement;
+  }
+
+  return null;
 }
 
 main();
