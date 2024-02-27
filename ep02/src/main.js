f2d3a934e741880b7081ea0d088407a6090116e9
obzva <dngyng1000@gmail.com>
2024-02-27 13:52:05 +0900

    Ep 02 - Part 1 - (7 of 7) Clean up, Rule of three

--- ep02/src/main.js
+++ ep02/src/main.js
@@ -1,17 +1,68 @@
 import test from "./test.json?raw";
 
 async function main() {
+  // fetch products
   const products = await getProducts();
+
+  // map id to product
   const productMap = {};
   products.forEach((p) => {
     productMap[p.id] = p;
   });
+
+  // map id to count
   const countMap = {};
 
+  // render product cards
   document.querySelector("#products").innerHTML = products
     .map((p) => getProductHTML(p, countMap[p.id]))
     .join("");
 
+  // util functions
+  function increaseCount(productId) {
+    if (countMap[productId] === undefined) countMap[productId] = 0;
+    countMap[productId]++;
+    console.log("🍏 increase!");
+    updateProductCount(productId);
+    updateCart();
+  }
+
+  function decreaseCount(productId) {
+    if (countMap[productId] === undefined) countMap[productId] = 0;
+    if (countMap[productId] > 0) {
+      countMap[productId]--;
+      if (countMap[productId] === 0) delete countMap[productId];
+      console.log("🍎 decrease!");
+    }
+    updateProductCount(productId);
+    updateCart();
+  }
+
+  function updateProductCount(productId) {
+    const productElement = document.querySelector(
+      `.product[data-product-id="${productId}"]`
+    );
+    const cartCountElement = productElement.querySelector(".cart-count");
+
+    if (countMap[productId] === 0 || countMap[productId] === undefined) {
+      cartCountElement.innerHTML = null;
+    } else {
+      cartCountElement.innerHTML = countMap[productId];
+    }
+  }
+
+  function updateCart() {
+    const productIds = Object.keys(countMap);
+    document.querySelector(".cart-items").innerHTML = productIds
+      .map((id) => getProductHTML(productMap[id], countMap[id]))
+      .join("");
+
+    const total = sumAllCounts(countMap);
+    document.querySelector(".total-count").innerHTML =
+      total > 0 ? `(${total})` : null;
+  }
+
+  // add click event listenr on cards of #products
   document.querySelector("#products").addEventListener("click", (e) => {
     const targetElement = e.target;
     const productElement = findElement(targetElement, ".product");
@@ -21,51 +72,55 @@ async function main() {
       targetElement.matches(".btn-decrease") ||
       targetElement.matches(".btn-increase")
     ) {
-      if (countMap[productId] === undefined) countMap[productId] = 0;
-
       if (targetElement.matches(".btn-decrease")) {
-        if (countMap[productId] > 0) {
-          countMap[productId]--;
-          if (countMap[productId] === 0) delete countMap[productId];
-          console.log("🍎 decrease!");
-        }
+        decreaseCount(productId);
       } else if (targetElement.matches(".btn-increase")) {
-        countMap[productId]++;
-        console.log("🍏 increase!");
+        increaseCount(productId);
       }
-
-      const cartcount = productElement.querySelector(".cart-count");
-
-      if (countMap[productId] === 0 || countMap[productId] === undefined) {
-        cartcount.innerHTML = null;
-      } else {
-        cartcount.innerHTML = countMap[productId];
-
-        const productIds = Object.keys(countMap);
-        document.querySelector(".cart-items").innerHTML = productIds
-          .map((id) => getProductHTML(productMap[id], countMap[id]))
-          .join("");
-      }
-
-      const total = sumAllCounts(countMap);
-      document.querySelector(".total-count").innerHTML =
-        total > 0 ? `(${total})` : null;
     }
   });
 
+  // add click event listenr on cards of .cart-items
+  document.querySelector(".cart-items").addEventListener("click", (e) => {
+    const targetElement = e.target;
+    const productElement = findElement(targetElement, ".product");
+    const productId = productElement.getAttribute("data-product-id");
+
+    if (
+      targetElement.matches(".btn-decrease") ||
+      targetElement.matches(".btn-increase")
+    ) {
+      if (targetElement.matches(".btn-decrease")) {
+        decreaseCount(productId);
+      } else if (targetElement.matches(".btn-increase")) {
+        increaseCount(productId);
+      }
+    }
+  });
+
+  // add click events to handle cart layer
   document.querySelector(".btn-cart").addEventListener("click", () => {
     document.querySelector(".cart-layer").classList.remove("hidden");
+    document.querySelector(".cart-bg").classList.add("overflow-y-auto");
+    document.querySelector("body").classList.add("overflow-y-hidden");
   });
 
   document.querySelector(".btn-close-cart").addEventListener("click", () => {
     document.querySelector(".cart-layer").classList.add("hidden");
+    document.querySelector(".cart-bg").classList.remove("overflow-y-auto");
+    document.querySelector("body").classList.remove("overflow-y-hidden");
   });
 
   document.querySelector(".cart-dimmed-bg").addEventListener("click", () => {
     document.querySelector(".cart-layer").classList.add("hidden");
+    document.querySelector(".cart-bg").classList.remove("overflow-y-auto");
+    document.querySelector("body").classList.remove("overflow-y-hidden");
   });
 }
 
+// fetch function to get products
+//   in local, we don't fetch and use local json
+//   in prod, we actually fetch
 async function getProducts() {
   if (process.env.NODE_ENV === "development") {
     return JSON.parse(test);
@@ -76,6 +131,7 @@ async function getProducts() {
   return await response.json();
 }
 
+// find element that matches specified selector iteratively
 function findElement(startingElement, selector) {
   let currentElement = startingElement;
 
@@ -87,10 +143,12 @@ function findElement(startingElement, selector) {
   return null;
 }
 
+// get sum of counts
 function sumAllCounts(countMap) {
   return Object.values(countMap).reduce((acc, curr) => acc + curr, 0);
 }
 
+// get html string of product card
 function getProductHTML(product, count) {
   return `
   <div class="product" data-product-id="${product.id}"">
